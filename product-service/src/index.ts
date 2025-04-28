364243c57fe11c52924d7810f24c2dd29f5f297e
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-04-28 11:53:51 -0400

    Merge

--- product-service/src/index.ts
+++ product-service/src/index.ts
@@ -12,6 +12,13 @@ AppDataSource.initialize()
   .catch((err) => {
     console.error('Error during Data Source initialization', err);
   });
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
 
 
 app.post("/products", craeteProduct);
